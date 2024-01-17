90247df8ad35a0960bc3627a3fd19f5702f5ca2f
BozzleMay <ledley26@msn.com>
2024-01-17 20:54:16 +0000

    server change

--- server/index.js
+++ server/index.js
@@ -6,14 +6,14 @@ import dalleRoutes from './routes/dalle.routes.js';
 
 dotenv.config();
 
-const app= express();
+const app = express();
 app.use(cors());
-app.use(express.json({limig: "50mb"}));
+app.use(express.json({ limig: "50mb" }))
 
-app.use('/api/v1/dalle', dalleRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello World'});
-});
+  res.status(200).json({ message: "Hello from DALL.E" })
+})
 
-app.listen(8080, () => console.log('Server is running'));
+app.listen(8080, () => console.log('Server has started on port 8080'))
